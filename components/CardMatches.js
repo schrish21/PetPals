fc6e344b41bd2b49e12d9046dfe109c874709d93
jay <example0040@hotmail.com>
2021-04-05 00:49:46 +0700

    reset

---
+++ components/CardMatches.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Text, Image, Dimensions, TouchableOpacity } from 'react-native';
+import styles from '../assets/style.js';
+import CardStack, { Card } from 'react-native-card-stack-swiper';
+
+const CardMatches = ({
+  name,
+  image,
+  screen
+}) => {
+  
+  //styles
+  const screenWidth = Dimensions.get('window').width;
+
+  const style_Image = [
+    {
+      width: screen ? screenWidth / 2 - 25 : screenWidth - 85,
+      height: screen ? 170 : 350,
+      borderRadius: 0,
+      margin: screen ? -2 : 20
+    }
+  ];
+
+  const style_Name = [
+    {
+      color: '#363636',
+      fontSize: screen ? 15 : 30,
+      paddingTop: screen ? 11 : 13,
+      paddingBottom: screen ? 5 : 8,
+    }
+  ];
+
+  return (
+    <View style={styles.containerCardItem}>
+      <Image source={image} style={style_Image}/>
+      <Text style={style_Name}>{name}</Text>
+    </View>
+  );
+};
+
+export default CardMatches;
---
+++ containers/Matches.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import styles from '../assets/style.js';
+import {ScrollView, View, Text, TouchableOpacity, ImageBackground, FlatList} from 'react-native';
+import CardMatches from '../components/CardMatches';
+
+import firebase from 'firebase';
+require('firebase/firestore');
+import { connect } from 'react-redux';
+
+
+function Matches (props) {
+
+  const [user, setUser] = useState(null);
+  const [following, setFollowing] = useState(false)
+  const [usersMatched, setUsersMatched] = useState([])
+
+    useEffect(() => {
+        const { currentUser, posts } = props;
+
+        if (props.route.params.uid === firebase.auth().currentUser.uid) {
+            setUser(currentUser)
+        }
+        else {
+            firebase.firestore()
+                .collection("users")
+                .doc(props.route.params.uid)
+                .get()
+                .then((snapshot) => {
+                    if (snapshot.exists) {
+                        setUser(snapshot.data());
+                    }
+                    else {
+                        console.log('does not exist')
+                    }
+                })
+        }
+
+        if (props.following.indexOf(props.route.params.uid) > -1) {
+            setFollowing(true);
+        } else {
+            setFollowing(false);
+        }
+
+        firebase.firestore()
+          .collection('users')
+          .where('uid', 'in', props.following)
+          .get()
+          .then((snapshot) => {
+              let usersMatched = snapshot.docs.map(doc => {
+                  const data = doc.data();
+                  const id = doc.id;
+                  return { id, ...data }
+              });
+              setUsersMatched(usersMatched);
+        })
+
+    }, [props.route.params.uid, props.following])
+
+    console.log(props.following)
+    //console.log('folowing =' +following)
+    //console.log(usersMatched)
+
+  return (
+    <View style={styles.containerMatches}>
+    <ImageBackground source={require('../assets/images/bg.png')} style={styles.bg}>
+      <ScrollView>
+          <View style={styles.top}>
+          <Text style={styles.status}>Matches</Text>
+          </View>
+      </ScrollView>
+
+      <FlatList
+        numColumns={2}
+        data={usersMatched}
+        horizontal={false}
+
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <CardMatches
+              image={item.downloadURL===undefined || null ? require('../assets/images/blank-profile.webp'): {uri: item.downloadURL} }
+              name={item.name === firebase.auth().currentUser.uid ? null : item.name}
+              screen
+            />
+          </TouchableOpacity>
+        )}
+      />
+    </ImageBackground>
+    </View>
+  );
+};
+
+
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+  posts: store.userState.posts,
+  following: store.userState.following
+})
+export default connect(mapStateToProps, null)(Matches);
